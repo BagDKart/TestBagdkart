b96b82a815f665d631cd8915e57df44945b2dd78
baluk1219 <baluk1219>
2017-09-14 00:51:48 +0530

    cancel order vendor

--- app/handlers/vendor/cancelOrder.js
+++ app/handlers/vendor/cancelOrder.js
@@ -1,5 +1,23 @@
+const mongo = require("mongodb").MongoClient;
+const config = require("../../../config.js");
+const urL = config.database;
+
 const cancelOrder = (req, res) => {
-	res.json({message: "cancelling the order"});
+	mongo.connect(urL, (err, db) => {
+		if(err) return err;
+
+		console.log("connected successfully in cancel order");
+		
+		db.collection('vendororderconfirms')
+		.updateOne({orderID: req.body.orderId}, {$set:{status: false}})
+		.then((data)=> {
+			console.log("updated successfully");
+			res.json("values updates");
+		}).catch((err)=>{
+			console.log(err);
+		});
+	});
+	console.log({message: "cancelling the order"});
 };
 
 module.exports = {
