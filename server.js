7e270e57da47a22444c3add7156ba8b910ed9fee
baluk1219 <baluk1219>
2017-11-01 19:08:42 +0530

    changes for bagdkart bluemix

--- server.js
+++ server.js
@@ -1,53 +1,56 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const mongo = require("mongodb").MongoClient;
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
+const express = require("express"),
+	app = express(),
+	mongoose = require("mongoose"),
+	mongo = require("mongodb").MongoClient,
+	bodyParser = require("body-parser"),
+	morgan = require("morgan"),
+	api = require("./app/routes/api.js")(app, express),
+	cfenv = require("cfenv"),
+	appEnv = cfenv.getAppEnv(),
+	config = require("./config.js"),
+	port = config.port;
 
-const api = require("./app/routes/api.js")(app, express);
-const config = require("./config.js");
-const port = config.port;
+console.log(appEnv);
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-mongoose.connect(config.database, {
-	useMongoClient: true,
-}).then((data)=> {
-	console.log("connected to mongoose");
-}).catch((err)=> {
-	console.log("err in connecting to mongoose")
-});
+// mongoose.connect(config.database, {
+// 	useMongoClient: true,
+// }).then((data)=> {
+// 	console.log("connected to mongoose");
+// }).catch((err)=> {
+// 	console.log("err in connecting to mongoose")
+// });
 
-mongo.connect(config.database)
-	.then(()=> {
-		console.log(`the database is ${config.database}`);
-}).catch((err) => {
-	console.log("there is a error");
-});
-
-
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-app.use(bodyParser.json());
-
-app.use(morgan('dev'));
-
-app.use("/api", api);
-
-app.get('*', function(req, res){
-    res.send("hello bagdkart backend");
-});
+// mongo.connect(config.database)
+// 	.then(()=> {
+// 		console.log(`the database is ${config.database}`);
+// }).catch((err) => {
+// 	console.log("there is a error");
+// });
 
 
+// app.use(bodyParser.urlencoded({
+// 	extended: true
+// }));
 
-app.listen(port, function(err) {
-	if(err) {
-		console.log("error in listening");
-	} else {
-		console.log(`listening to port ${port}`);
-	}
-});
+// app.use(bodyParser.json());
+
+// app.use(morgan('dev'));
+
+// app.use("/api", api);
+
+// app.get('*', function(req, res){
+//     res.send("hello bagdkart backend");
+// });
+
+
+
+// app.listen(port, function(err) {
+// 	if(err) {
+// 		console.log("error in listening");
+// 	} else {
+// 		console.log(`listening to port ${port}`);
+// 	}
+// });
 
